9e0127adf7351a90d46803c96e5e278c01b6a8e5
dennis <dgroen@virtualciences.nl>
2024-10-31 18:37:20 +0000

    Fixed SagaService Strartup

--- MessageBrokers/RabbitMQConfig.cs
+++ MessageBrokers/RabbitMQConfig.cs
@@ -3,7 +3,7 @@
     public class RabbitMQConfig
     {
         public const string RabbitMQURL = "rabbitmq://localhost";
-        public const string UserName = "guest";
-        public const string Password = "guest";
+        public const string UserName = "<username>";
+        public const string Password = "<password>";
     }
 }
